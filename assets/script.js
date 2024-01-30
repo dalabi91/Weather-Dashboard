e7c2c463721c2edc58e260c4f2123a57433cc6c6
dalabi91 <152018805+dalabi91@users.noreply.github.com>
2024-01-30 18:34:01 +0000

    edited css & started on js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
-const weatherUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-const apiKey = b561e9c149f3178a4909a7f097b91445;
+$(document).ready(function () {
+  const weatherUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+  //open weather APIkey
+  const apiKey = "b561e9c149f3178a4909a7f097b91445";
+  // event listener for teh submit key
+  $("#search-form").submit(function (event) {
+    event.preventDefault();
+    console.log();
+  });
+});
