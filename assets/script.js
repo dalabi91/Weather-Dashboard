9c494f0672f210ab862368f4258895f20f1f5c7b
dalabi91 <152018805+dalabi91@users.noreply.github.com>
2024-01-30 23:07:49 +0000

    code to hide current weather card until search is made added

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
+  //hide weather info card until a search is done
+  $(".weatherInfo").css("display", "none");
+
   const weatherUrl =
     "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
   //open weather APIkey
   const apiKey = "b561e9c149f3178a4909a7f097b91445";
   // event listener for teh submit key
-  $("#search-form").submit(function (event) {
-    event.preventDefault();
-    console.log();
-  });
+  // $("#search-form").submit(function (event) {
+  //   event.preventDefault();
+  //   console.log();
+  // });
 });
